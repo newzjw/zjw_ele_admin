eac20467f1a125fc15e973f0c378dbac22e9e838
zjw <jiangweizhu713@163.com>
2020-11-01 08:23:59 +0800

    项目完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,75 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-import Login from '../components/Login.vue'
+// import Login from '../components/Login.vue'
+// import Home from '../components/Home.vue'
+// import Welcome from '../components/Welcome.vue'
+
+// import Users from '../components/user/User.vue'
+// import Rights from '../components/power/Rights.vue'
+// import Roles from '../components/power/Roles.vue'
+
+// import Cate from '../components/goods/Cate.vue'
+// import Params from '../components/goods/Params.vue'
+
+// import GoodsList from '../components/goods/List.vue'
+// import Add from '../components/goods/Add.vue'
+
+// import Order from '../components/order/Order.vue'
+// import Report from '../components/report/Report.vue'
+
+// 路由懒加载
+const Login = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Login.vue')
+const Home = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Home.vue')
+const Welcome = () => import(/* webpackChunkName: "Login_Home_Welcome" */ '../components/Welcome.vue')
+
+const Users = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/user/User.vue')
+const Rights = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Rights.vue')
+const Roles = () => import(/* webpackChunkName: "Users_Rights_Roles" */ '../components/power/Roles.vue')
+
+const Cate = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Cate.vue')
+const Params = () => import(/* webpackChunkName: "Cate_Params" */ '../components/goods/Params.vue')
+
+const GoodsList = () => import(/* webpackChunkName: "GoodsList_Add" */ '../components/goods/List.vue')
+const Add = () => import(/* webpackChunkName: "GoodsList_Add" */ '../components/goods/Add.vue')
+
+const Order = () => import(/* webpackChunkName: "Order_Report" */ '../components/order/Order.vue')
+const Report = () => import(/* webpackChunkName: "Order_Report" */ '../components/report/Report.vue')
+
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+  { path: '/Home', 
+    component: Home,
+    // redirect: '/Welcome',
+    children: [
+      { path: '/home', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/rights', component: Rights },
+      { path: '/roles', component: Roles },
+      { path: '/goods', component: GoodsList },
+      { path: '/categories', component: Cate },
+      { path: '/params', component: Params },
+      { path: '/goods/add', component: Add },
+      { path: '/orders', component: Order },
+      { path: '/reports', component: Report }
+    ] 
+  }
 ]
 const router = new VueRouter({
   routes
+})
+
+// 在路由配置生效之前，统一判断token
+// 挂载路由导航守卫,to表示将要访问的路径，from表示从哪里来，next是下一个要做的操作 next('/login')强制跳转login
+router.beforeEach((to, from, next) => {
+  // 如果访问登录页，放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = localStorage.getItem('token')
+  // 没有token, 强制跳转到登录页
+  if (!tokenStr) return next('/login')
+  next() // 自动调用下一个中间件，这里是让路由配置执行
 })
 
 export default router
